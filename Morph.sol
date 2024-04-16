c4f30d9059d426471db2d9230167f32299e1c517
None + None = None <SwartzMss@users.noreply.github.com>
2024-04-16 10:46:33 +0800

    Update Morph.sol

--- Morph.sol
+++ Morph.sol
@@ -2,12 +2,25 @@
 pragma solidity ^0.8.4;
 
 contract MorphDecoder {
-    function decodeABIForDepositETHTo(bytes calldata datainfo) external pure returns (address _to,uint32 _l2Gas,bytes memory _data) {
+    function d1ecodeABIForDepositETHToTest(bytes calldata datainfo) external pure returns (uint32 _l2Gas) {
+         (_l2Gas) = abi.decode(datainfo, (uint32));
+        return (_l2Gas);
+    }
+
+
+    function d2ecodeABIForDepositETHTo(bytes calldata datainfo) external pure returns (uint32 _l2Gas,bytes memory _data) {
+         (_l2Gas,_data) = abi.decode(datainfo, (uint32,bytes));
+        return (_l2Gas,_data);
+    }
+
+    function d3ecodeABIForDepositETH(bytes calldata datainfo) external pure returns (address _to,uint32 _l2Gas,bytes memory _data) {
          (_to,_l2Gas,_data) = abi.decode(datainfo, (address,uint32,bytes));
         return (_to,_l2Gas,_data);
     }
-    // Define a structure to hold details of a batch swap step
-    struct BatchSwapStep {
+
+    event LogTxData(uint256 nonce, address sender, address target, uint256 value, uint256 gasLimit, bytes userData);
+
+    struct _tx {
         uint256 nonce;
         address sender;
         address target;
@@ -16,15 +29,25 @@ contract MorphDecoder {
         bytes userData;
     }
 
-    // Function to decode the ABI data for a 'finalizeWithdrawalTransaction'
-    function decodeABIForfinalizeWithdrawalTransaction(bytes calldata datainfo)
+
+    function d4ecodeABIForfinalizeWithdrawalTransaction(bytes calldata datainfo)
         external
-        pure
-        returns (BatchSwapStep[] memory txInfo)
+        returns (_tx memory txInfo)
     {
-        // Decode the incoming data into an array of BatchSwapStep structs
-        txInfo = abi.decode(datainfo, (BatchSwapStep[]));
+        txInfo = abi.decode(datainfo, (_tx));
+        emit LogTxData(txInfo.nonce, txInfo.sender, txInfo.target, txInfo.value, txInfo.gasLimit, txInfo.userData);
         return txInfo;
     }
+
+    function d5ecodeABIForproveWithdrawalTransaction(bytes calldata data)
+        external
+        returns (_tx memory transaction, bytes32[32] memory withdrawalProof, bytes32 withdrawalRoot)
+    {
+        // 解码传入的数据
+        (transaction, withdrawalProof, withdrawalRoot) = abi.decode(data, (_tx, bytes32[32], bytes32));
+        emit LogTxData(transaction.nonce, transaction.sender, transaction.target, transaction.value, transaction.gasLimit, transaction.userData);
+        return (transaction, withdrawalProof, withdrawalRoot);
+    }
+
 
 }
